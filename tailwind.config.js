7018872c8d095bab248634ccad68b845a0e06674
Moynul Islam <moynul.officials@gmail.com>
2023-06-01 00:52:00 +0600

    4th commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,21 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
-    color: {
-      "brandClr": "#212e50"
-
+    colors: {
+      'dark': '#212e50',
+      'light': '#d1d5db',
+      'bg_black': '#000',
+      'text_light': '#fff',
     },
+    fontSize: {
+      'text_logo': '34px',
+      'xsm_text': '12px',
+      'sm_text': '14px',
+      'sm_icon': '8px',
+      'md_icon': '20px',
+      'lg_icon': '30px'
+    },
+
   },
   plugins: [],
 }
